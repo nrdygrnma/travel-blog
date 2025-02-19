ff7d68b705e7b1e4fe64f66f12807f696ec86e23
nrdygrnma <github@nrdygrnma.tech>
2025-02-19 13:23:23 +0100

    Added content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxt/content"],
+  content: {
+    database: {
+      type: "postgres",
+      url: process.env.POSTGRES_URL!,
+    },
+    preview: {
+      api: "https://api.nuxt.studio",
+    },
+  },
+});
